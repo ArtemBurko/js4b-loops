794ae91d0ec398e0f01d78db836bbd1585564af9
ArtemBurko <44812012+ArtemBurko@users.noreply.github.com>
2018-11-28 23:22:21 +0200

    Update task.js

--- for-loop/task.js
+++ for-loop/task.js
@@ -1,9 +1,19 @@
 function listAll(min, max) {
-  var result = '1 2 3 ';
+  var i; 
+  var result = '';
+  for (i = min; i <= max; i++) {
+    result = result + i + ' ';}
+    // console.log(result);}
+  
   return result.trim();
+
 }
 
 function listBetweenDesc(min, max) {
-  var result = '3 2';
+  var i; 
+  var result = '';
+  for (i = max-1; i > min; i--) {
+    result = result + i + ' ';}
+    // console.log(result);}
   return result.trim();
 }
